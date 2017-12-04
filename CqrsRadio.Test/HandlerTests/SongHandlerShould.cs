3aa55ad77068e7e321aa894b0c8ec16183916d58
dublow <nicolas.dfr@gmail.com>
2017-12-04 17:12:46 +0100

    Refactor

--- CqrsRadio.Test/HandlerTests/SongHandlerShould.cs
+++ CqrsRadio.Test/HandlerTests/SongHandlerShould.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
-using CqrsRadio.Domain.EventHandlers;
+﻿using System.Linq;
 using CqrsRadio.Domain.Events;
 using CqrsRadio.Domain.ValueTypes;
-using Moq;
+using CqrsRadio.Handlers;
+using CqrsRadio.Test.Mocks;
 using NUnit.Framework;
 
 namespace CqrsRadio.Test.HandlerTests
@@ -31,50 +26,6 @@ namespace CqrsRadio.Test.HandlerTests
             Assert.AreEqual(playlistName, actualPlaylistName);
             Assert.AreEqual(title, actualTitle);
             Assert.AreEqual(artist, actualArtist);
-        }
-    }
-
-    public class SongHandler : ISongHandler
-    {
-        private readonly ISongRepository _songRepository;
-
-        public SongHandler(ISongRepository songRepository)
-        {
-            _songRepository = songRepository;
-        }
-
-        public void Handle(SongAdded evt)
-        {
-            _songRepository.Add(evt.UserId, evt.PlaylistName, evt.Title, evt.Artist);
-        }
-    }
-
-    public interface ISongRepository
-    {
-        void Add(UserId userId, string playlistName, string title, string artist);
-    }
-
-    public class SongRepositoryBuilder
-    {
-        private readonly Mock<ISongRepository> _mock;
-        public readonly List<(UserId userId, string playlistName, string title, string artist)> Songs;
-
-        public SongRepositoryBuilder()
-        {
-            _mock = new Mock<ISongRepository>();
-            Songs = new List<(UserId userId, string playlistName, string title, string artist)>();
-        }
-        public static SongRepositoryBuilder Create()
-        {
-            return new SongRepositoryBuilder();
-        }
-
-        public ISongRepository Build()
-        {
-            _mock.Setup(x => x.Add(It.IsAny<UserId>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
-                .Callback<UserId, string, string, string>((userId, name, title, artist) => Songs.Add((userId, name, title, artist)));
-
-            return _mock.Object;
         }
     }
 }
