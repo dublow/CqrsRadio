1b9018226a842f167649fb47e37d245b1f8d9f85
dublow <nicolas.dfr@gmail.com>
2017-12-04 14:49:30 +0100

    Add ISongHandler to domain

---
+++ CqrsRadio.Domain/EventHandlers/ISongHandler.cs
@@ -0,0 +1,9 @@
+﻿using CqrsRadio.Domain.Events;
+using CqrsRadio.Domain.Handlers;
+
+namespace CqrsRadio.Domain.EventHandlers
+{
+    public interface ISongHandler :
+        IHandler<SongAdded>
+    { }
+}
