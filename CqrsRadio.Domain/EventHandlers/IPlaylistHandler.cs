702d2005a956b51ef4d4af96eb96b698a4c9face
dublow <nicolas.dfr@gmail.com>
2017-12-04 14:21:56 +0100

    Add IPlaylistHandler to domain

---
+++ CqrsRadio.Domain/EventHandlers/IPlaylistHandler.cs
@@ -0,0 +1,10 @@
+﻿using CqrsRadio.Domain.Events;
+using CqrsRadio.Domain.Handlers;
+
+namespace CqrsRadio.Domain.EventHandlers
+{
+    public interface IPlaylistHandler<in TEvent> : 
+        IHandler<PlaylistAdded>, 
+        IHandler<PlaylistCreated> where TEvent : IDomainEvent
+    { }
+}
