ba6464611df0f6d54f778d2bc008d6f10ca8eee4
dublow <nicolas.dfr@gmail.com>
2017-12-04 14:47:31 +0100

    Add IRadioSongHandler to domain

---
+++ CqrsRadio.Domain/EventHandlers/IRadioSongHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CqrsRadio.Domain.Events;
+using CqrsRadio.Domain.Handlers;
+
+namespace CqrsRadio.Domain.EventHandlers
+{
+    public interface IRadioSongHandler<in TEvent> :
+        IHandler<RadioSongParsed>,
+        IHandler<RadioSongDuplicate>,
+        IHandler<RadioSongError>
+        where TEvent : IDomainEvent
+    {
+    }
+}
