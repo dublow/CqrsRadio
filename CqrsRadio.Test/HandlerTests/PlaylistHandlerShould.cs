f2298d86e14a3e7ac1ca58b360ecbc78e0acd57b
dublow <nicolas.dfr@gmail.com>
2017-12-04 15:29:12 +0100

    pass to green test

--- CqrsRadio.Test/HandlerTests/PlaylistHandlerShould.cs
+++ CqrsRadio.Test/HandlerTests/PlaylistHandlerShould.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using CqrsRadio.Domain.EventHandlers;
 using CqrsRadio.Domain.Events;
+using CqrsRadio.Domain.ValueTypes;
+using Moq;
 using NUnit.Framework;
 
 namespace CqrsRadio.Test.HandlerTests
@@ -23,24 +26,63 @@ namespace CqrsRadio.Test.HandlerTests
         }
     }
 
-    public class PlaylistEntity
+    public class PlaylistEntity : IEquatable<PlaylistEntity>
     {
-        public PlaylistEntity(string userId, string playlistName)
+        public UserId UserId;
+        public string Name;
+
+        public PlaylistEntity(UserId userId, string name)
         {
-            throw new System.NotImplementedException();
+            UserId = userId;
+            Name = name;
+        }
+
+        public bool Equals(PlaylistEntity other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return UserId.Equals(other.UserId) && string.Equals(Name, other.Name);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((PlaylistEntity) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (UserId.GetHashCode() * 397) ^ (Name != null ? Name.GetHashCode() : 0);
+            }
+        }
+
+        public static bool operator ==(PlaylistEntity left, PlaylistEntity right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(PlaylistEntity left, PlaylistEntity right)
+        {
+            return !Equals(left, right);
         }
     }
 
     public class PlaylistHandler : IPlaylistHandler
     {
+        private readonly IPlaylistRepository _playlistRepository;
+
         public PlaylistHandler(IPlaylistRepository playlistRepository)
         {
-            throw new System.NotImplementedException();
+            _playlistRepository = playlistRepository;
         }
 
         public void Handle(PlaylistAdded evt)
         {
-            throw new System.NotImplementedException();
+            _playlistRepository.AddPlaylist(evt.UserId, evt.Name);
         }
 
         public void Handle(PlaylistDeleted evt)
@@ -51,20 +93,34 @@ namespace CqrsRadio.Test.HandlerTests
 
     public interface IPlaylistRepository    
     {
+        void AddPlaylist(UserId userId, string name);
     }
 
     public class PlaylistRepositoryBuilder
     {
+        public PlaylistRepositoryBuilder()
+        {
+            _mock = new Mock<IPlaylistRepository>();
+            Playlists = new List<PlaylistEntity>();
+        }
         public static PlaylistRepositoryBuilder Create()
         {
-            throw new System.NotImplementedException();
+            return new PlaylistRepositoryBuilder();
         }
 
         public IPlaylistRepository Build()
         {
-            throw new System.NotImplementedException();
+            _mock.Setup(x => x.AddPlaylist(It.IsAny<UserId>(), It.IsAny<string>()))
+                .Callback<UserId, string>((userId, name) =>
+                {
+                    Playlists.Add(new PlaylistEntity(userId, name));
+                });
+                   
+
+            return _mock.Object;
         }
 
         public readonly List<PlaylistEntity> Playlists;
+        private Mock<IPlaylistRepository> _mock;
     }
 }
