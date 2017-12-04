62a470e681218d4cb59c449268aebdc148bad81d
dublow <nicolas.dfr@gmail.com>
2017-12-04 14:23:54 +0100

    Add IRadioHandler to domain

---
+++ CqrsRadio.Domain/EventHandlers/IRadioHandler.cs
@@ -0,0 +1,10 @@
+﻿using CqrsRadio.Domain.Events;
+using CqrsRadio.Domain.Handlers;
+
+namespace CqrsRadio.Domain.EventHandlers
+{
+    interface IRadioHandler<in TEvent> :
+        IHandler<RadioCreated>,
+        IHandler<RadioDeleted> where TEvent : IDomainEvent
+    { }
+}
