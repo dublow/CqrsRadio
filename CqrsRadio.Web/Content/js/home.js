3889ee46a5d368b0f8bf1fcf5497116f5b5c95c7
dublow <nicolas.dfr@gmail.com>
2017-12-06 14:33:11 +0100

    Add login logout deezer

---
+++ CqrsRadio.Web/Content/js/home.js
@@ -0,0 +1,57 @@
+﻿var home = (function () {
+    var deezer = {
+        getLoginStatus: (model) => {
+            DZ.getLoginStatus(function (response) {
+                if (response.authResponse) {
+                    DZ.api('/user/me', function (response) {
+                        if (response.error)
+                            return;
+                        model.loginText("Hi " + response.name);
+                        model.isLogged(true);
+                    });
+                } else {
+                    console.log('User cancelled login or did not fully authorize.');
+                }
+            });
+        },
+        login: (model) => {
+            if (model.isLogged())
+                deezer.logout(model);
+            else {
+                DZ.login(function (response) {
+                    if (response.authResponse) {
+                        DZ.api('/user/me', function (response) {
+                            model.loginText("Hi " + response.name);
+                            model.isLogged(true);
+                        });
+                    } else {
+                        console.log('User cancelled login or did not fully authorize.');
+                    }
+                }, { perms: 'basic_access,email' });
+            }
+           
+        },
+        logout: (model) => {
+            DZ.logout();
+            model.loginText("Signin");
+            model.isLogged(false);
+        }
+    };
+    var public = {
+        info: () => console.log("Welcome home!"),
+        model: {
+            isLogged: ko.observable(false),
+            loginText: ko.observable('Signin'),
+            init: () => deezer.getLoginStatus(public.model),
+            login: deezer.login
+        }
+    };
+
+    ko.applyBindings(public.model);
+    return public;
+})();
+
+$(document).ready(function () {
+    home.model.init();
+    home.info();
+});
